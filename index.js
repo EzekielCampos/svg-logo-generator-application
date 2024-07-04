4666a72a96cfe80b8fd5f9db8e881c76b67ea852
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-07-04 16:31:37 -0700

    created inquirer prompts to get user responses for logo

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 
 const {Circle, Triangle, Square} = require("./lib/shapes");
 
+const inquirer = require("inquirer");
+
 const testShape = new Circle("red", "orange", "magenta");
 
 
-console.log(testShape.type);
+
+
+
+inquirer.prompt([
+    {
+        type:"input",
+        message:"Enter three letters that you would like for your logo",
+        name:"logo",
+    },
+    {
+        type:"input",
+        message:"Enter a color or hexadecimal value that you would like for the text color",
+        name:"textColor",
+    },
+
+    {
+        type:"list",
+        message:"Enter the shape you would like to have for the logo",
+        name:"shape",
+        choices:["Circle", "Square", "Triangle"],
+    },
+    {
+        type:"input",
+        message:"Enter a color or hexadecimal value that you would like for the shape",
+        name:"shapeColor",
+    },
+    
+]).then(response =>{
+
+
+console.log(response);
+
+})
+
 
 
